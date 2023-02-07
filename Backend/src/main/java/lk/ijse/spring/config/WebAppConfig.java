74c8d4c31e3a50f33d98a3514fdc89f355c80b91
Sandun Induranga <sandutharu40@gmail.com>
2023-02-07 23:58:25 +0530

    Created File Controller

--- Backend/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Backend/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.controller.FileController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,6 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan
+@ComponentScan(basePackageClasses = {FileController.class})
 public class WebAppConfig {
 }
