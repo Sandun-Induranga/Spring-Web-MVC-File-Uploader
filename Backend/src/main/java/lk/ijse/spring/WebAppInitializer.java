984c10ce9abefad2a9956af30df8f04248ef355d
Sandun Induranga <sandutharu40@gmail.com>
2023-02-08 00:00:56 +0530

    Refactored WebAppInitializer

--- Backend/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ Backend/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,8 +1,25 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppInitializer.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
