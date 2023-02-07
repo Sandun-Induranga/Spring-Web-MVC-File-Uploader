a03229fc43df9a873c2b5227aec08bbc0434efe8
Sandun Induranga <sandutharu40@gmail.com>
2023-02-08 00:28:56 +0530

    Created Ajax Method

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,4 +1,13 @@
 $("button").on("click", function () {
     console.log($("input:first-child").val());
 
+    $.ajax({
+        url:"http://localhost:8080/uploader/upload",
+        method:"post",
+        data:$("input:first-child").val(),
+        success:function () {
+            alert("Done");
+        }
+    })
+
 });
