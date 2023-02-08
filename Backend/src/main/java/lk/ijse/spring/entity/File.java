31ae679c0b269511cb22e40047f02bd936892299
Sandun Induranga <sandutharu40@gmail.com>
2023-02-08 11:29:51 +0530

    File Entity Created

---
+++ Backend/src/main/java/lk/ijse/spring/entity/File.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class File {
+
+    @Id
+    @GeneratedValue
+    String id;
+    String fileString;
+    String fileName;
+
+}
