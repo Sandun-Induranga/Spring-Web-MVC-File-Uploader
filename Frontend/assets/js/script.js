296c17faa0eaa1d86353721ee4f274b16173b12e
Sandun Induranga <sandutharu40@gmail.com>
2023-02-10 21:18:29 +0530

    Content Updated

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,3 +1,8 @@
+$("aside>input:first-child").change(function () {
+    console.log("Invoked")
+    $("aside>input:last-child").val($("aside>input:first-child").files[0].name)
+});
+
 $("button").on("click", function () {
 
     var data = new FormData($("#file-form")[0]);
@@ -12,6 +17,6 @@ $("button").on("click", function () {
         success: function (res) {
             $('#image').attr('src', `data:image/png;base64,${res}`);
         }
-    })
+    });
 
 });
